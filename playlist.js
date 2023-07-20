92606bdf86093b47f371fceb55a0cd09bc25fd77
Shaista Afreen <97823364+ShaistaAfreen09@users.noreply.github.com>
2023-07-20 12:30:17 +0530

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,28 @@
+let songs = [
+  {
+    name: "Blinding Lights - The Weekend",
+    artist: "The Weekend",
+    img: "img5",
+    audio : "music1"
+
+  },
+  {
+    name: "Call out my name -The Weekend",
+    artist : "The Weekend",
+    img: "img6",
+    audio: "music2"
+   
+  },
+  {
+    name: "How do I make you love me? -The Weekend",
+    artist : "The Weekend",
+    img: "img7",
+    audio: "music3"
+  },
+  {
+    name: "Save your tears - The Weekend",
+    artist:" The Weekend",
+    img : "img8",
+    audio:"music4"
+  }
+]
